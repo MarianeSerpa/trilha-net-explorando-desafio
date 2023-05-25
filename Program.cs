bbecfc024170e32d89b41124deec7aab3018771c
Mariane <mariane@gmail.com>
2023-05-25 10:23:11 -0300

    feat : Inclusão das validações na classe Reserva

--- Program.cs
+++ Program.cs
@@ -13,13 +13,13 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 50);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 5);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+// Exibe a quantidade de hóspedes e o valor da diáriadotn
+Console.WriteLine($"Hospedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor diaria: {reserva.CalcularValorDiaria()}");
